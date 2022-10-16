7120a26792bceebd9e1bdc806a962e4aaed14915
urlunaticguy <85105608+urlunaticguy@users.noreply.github.com>
2022-10-16 21:48:28 +0530

    enlarged home imgs

--- src/Components/HomeImage.jsx
+++ src/Components/HomeImage.jsx
@@ -4,7 +4,7 @@ function HomeImage(props) {
   return (
     <div className=" border-2 border-white flex flex-col justify-center items-center mb-4">
       <img
-        className=" w-[18rem] h-[14rem] p-2 blur-[2px] opacity-80"
+        className=" w-[18rem] h-[14rem] lg:w-[30rem] lg:h-[26rem] p-2 blur-[2px] opacity-80"
         src={props.img}
       ></img>
       <button
