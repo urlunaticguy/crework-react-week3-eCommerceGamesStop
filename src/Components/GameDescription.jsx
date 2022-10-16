e7719ab383639c5d9b5eb573a1dced69f85b8f38
urlunaticguy <85105608+urlunaticguy@users.noreply.github.com>
2022-10-16 18:05:32 +0530

    changed style of large screen game desc page

--- src/Components/GameDescription.jsx
+++ src/Components/GameDescription.jsx
@@ -5,31 +5,33 @@ function GameDescription(props) {
   return (
     <div
       id={gameDataObj[props.indexx].id}
-      className=" flex flex-col justify-center items-center mb-8"
+      className=" flex flex-col lg:flex-row justify-center items-center mb-8"
     >
       <img
-        className=" w-[15rem] h-[15rem] lg:w-[18rem] lg:h-[18rem] border-4 border-white"
+        className=" w-[16rem] h-[16rem] lg:w-[24rem] lg:h-[20rem] border-4 border-white"
         src={gameDataObj[props.indexx].img}
       />
-      <label className=" text-[1.4rem] mt-2">
-        {gameDataObj[props.indexx].name}
-      </label>
-      <label className=" text-[2.5rem]">
-        ${gameDataObj[props.indexx].price[0]}.
-        {gameDataObj[props.indexx].price[1]}
-      </label>
-      <button
-        onClick={props.cartAdd}
-        className=" cursor-pointer px-2 py-1 mb-2 rounded-lg bg-gradient-to-r from-[#75b022] to-[#588b1a] font-semibold hover:underline"
-      >
-        Add to Cart
-      </button>
-      <label className=" text-[0.9rem]">
-        Developer : {gameDataObj[props.indexx].dev}
-      </label>
-      <label className=" text-[0.9rem]">
-        Year : {gameDataObj[props.indexx].year}
-      </label>
+      <div className=" flex flex-col lg:ml-8 lg:justify-between lg:h-[18rem]">
+        <label className=" text-[1.4rem] mt-2 lg:text-[2rem]">
+          {gameDataObj[props.indexx].name}
+        </label>
+        <label className=" text-[2.5rem]">
+          ${gameDataObj[props.indexx].price[0]}.
+          {gameDataObj[props.indexx].price[1]}
+        </label>
+        <button
+          onClick={props.cartAdd}
+          className=" cursor-pointer px-2 py-1 mb-2 rounded-lg bg-gradient-to-r from-[#75b022] to-[#588b1a] font-semibold hover:underline"
+        >
+          Add to Cart
+        </button>
+        <label className=" text-[0.9rem]">
+          Developer : {gameDataObj[props.indexx].dev}
+        </label>
+        <label className=" text-[0.9rem]">
+          Year : {gameDataObj[props.indexx].year}
+        </label>
+      </div>
     </div>
   );
 }
